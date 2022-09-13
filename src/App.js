78e37d279b6283029fdfd2ebf424d33eed30a449
zach <zacharie.bugaud@grenoble-inp.org>
2022-09-13 16:28:14 +0200

    add workflow

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
 
+import TextField from "@mui/material/TextField";
+import './App.css';
+import ResponseList from "./ResponseList";
+import { React,  useState } from 'react'
+
+import Divider from '@mui/material/Divider';
 function App() {
+
+
+  const [wordList, setWordList] = useState([]);
+
+  let getWords = (definition) => {
+    //convert input text to lower case
+ fetch('http://127.0.0.1:8000',{
+      method: 'POST',
+      body: JSON.stringify({
+        definition:definition
+      }),
+      headers: {
+        'Content-type': 'application/json;',
+      }
+      }).then(function(response){ 
+        return response.json()})
+        .then(function(data)
+        {
+          setWordList(data)
+
+      }).catch(error => console.error('Error:', error))
+
+  }
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Le bon mot</h1>
+        <h6>Trouvez un mot à partir de sa définition (approximative)</h6>
+
+
+      <div className="search">
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          fullWidth
+          label="Search"
+
+          onKeyPress={(ev) => {
+            if (ev.key === 'Enter') {
+              // Do code here
+              getWords(ev.target.value)
+              ev.preventDefault();
+            }
+          }}
+
+
+          size="small"
+          inputProps={{ maxLength: 200 }}          
+        />
+      </div>
+      <Divider variant="inset" component="li" />
+      { wordList.length>0 &&
+        <ResponseList words={wordList}  />
+      } 
+      
       </header>
+      
     </div>
   );
 }
---
+++ src/ResponseList.js
@@ -0,0 +1,56 @@
+
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Typography from '@mui/material/Typography';
+
+export default function ResponseList({words}) {
+    //create a new array by filtering the original array
+
+    /*
+    return (
+        <ul>
+            {words.map((item) => (
+
+                <li>
+                    <p>{item[1]}</p>
+                    <p>{item[0]}</p>
+                </li>
+            ))}
+        </ul>
+    )
+    */
+
+    return (
+
+        <List sx={{ width: '70%',  bgcolor: 'background.paper' }}>
+
+{words.map((item) => (
+        <ListItem alignItems="flex-start" button component="a" href={"https://fr.wiktionary.org/wiki/"+item[0]} target="_blank">
+  
+  
+          <ListItemText
+            primary={item[0]}
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{ display: 'inline' }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  {item[1]}
+                </Typography>
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+
+))}
+     
+      </List>
+
+    )
+}
+
