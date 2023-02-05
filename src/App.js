8565975843675093de0c455d140cd28b5b06f263
wd400 <87280280+wd400@users.noreply.github.com>
2023-02-05 15:34:47 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
 
     setWordList([])
     //convert input text to lower case
- fetch('https://ad90-81-64-42-202.eu.ngrok.io/tgw',{
+ fetch('https://724e-81-64-42-202.eu.ngrok.io/tgw',{
       method: 'POST',
       body: JSON.stringify({
         definition:definition,
